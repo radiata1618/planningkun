0e06bbfa52f51a72942029311781e1170c8191ae
radiata1618 <haruki.u@gmail.com>
2022-02-10 20:40:15 +0900

    トピックページの土台は作成完了

--- lib/routes/topic_route.dart
+++ lib/routes/topic_route.dart
@@ -24,116 +24,138 @@ class Topic extends StatefulWidget {
 class _Topic extends State<Topic> {
   static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
   static FirebaseAnalyticsObserver observer =
-  FirebaseAnalyticsObserver(analytics: analytics);
+      FirebaseAnalyticsObserver(analytics: analytics);
 
   List<Widget> workCategoryList = [];
   bool initialProcessflg = true;
 
-
   FirebaseStorage storage = FirebaseStorage.instance;
-
 
   @override
   Widget build(BuildContext context) {
-
-    if(initialProcessflg){
-      initialProcessflg=false;
+    if (initialProcessflg) {
+      initialProcessflg = false;
       setCategoryLines();
-
     }
     return Scaffold(
         body: SafeArea(
             child: Container(
                 child: Column(children: [
-                  Container(color: Colors.black54, height: 40),
-                  Expanded(
-                    child: topicsEntire(),
-                  )
-                ]))));
+      Container(color: Colors.black54, height: 40),
+      Expanded(
+        child: topicsEntire(),
+      )
+    ]))));
   }
 
   Widget topicsEntire() {
     return SingleChildScrollView(
         child: Column(
-          children: horizonalDetailList(),
-        ));
+          children:[
+          Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("Topic",
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 16,
+                        color: Colors.black45,
+                      ),
+                    )),
+              )),
+            Container(child:Column(children:horizonalDetailList(),))
+            ]
+    ));
   }
 
   List<Widget> horizonalDetailList() {
-
     final List<Widget> outputList = workCategoryList;
 
-    if(outputList.length==0){
+    if (outputList.length == 0) {
       return [Text("")];
-    }else{
-
+    } else {
       return outputList;
     }
-
   }
 
   Future<void> setCategoryLines() async {
-
-
-    // await setCategoryUnit("food");
+    await setCategoryUnit("food");
     await setCategoryUnit("nature");
+    //TODO どのカテゴリを表示するか、DBから設定値を取得
     // await setCategoryUnit("religion");
 
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   Future<void> setCategoryUnit(String categoryName) async {
     List<Widget> topicList = [];
 
-    await FirebaseFirestore.instance.collection('topics')
+    await FirebaseFirestore.instance
+        .collection('topics')
         .where('categoryName', isEqualTo: categoryName)
-        .get().then((QuerySnapshot snapshot)async {
+        .get()
+        .then((QuerySnapshot snapshot) async {
+      // for(int i=0;i<snapshot.docs.length;i++){
 
+      for (int i = 0; i < snapshot.docs.length; i++) {
+        Reference imageRef =
+            await storage.ref(snapshot.docs[i].get("photoPath"));
+        String imageUrl = await imageRef.getDownloadURL();
+        Image img = await Image.network(imageUrl, height: 40, width: 70);
 
-      snapshot.docs.forEach((doc) async{
-        //
-        // Reference imageRef = await storage.ref(doc.get("photoPath"));
-        // String imageUrl = await imageRef.getDownloadURL();
-        // Image img =await Image.network(imageUrl,width:90);
+        topicList.add(Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 10),
+            child: Container(
+              color: Colors.white10,
+              height: 200,
+              width: 140,
+              child: Column(children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.white,
+                  backgroundImage: img.image,
+                ),
+                Text(
+                  snapshot.docs[i].get("topicName"),
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                    color: Colors.black45,
+                  ),
+                )
+              ]),
+            )));
+      }
+      ;
+    });
 
-        topicList.add(
-          // Padding(
-          //   padding:const EdgeInsets.symmetric(horizontal:4,vertical:3 ),
-          //   child:Container(
-          //     width:100,
-          //     child: Column(
-          //       children:[
-          //
-          //         // CircleAvatar(
-          //         //   radius: 90,
-          //         //   backgroundColor: Colors.white,
-          //         //   backgroundImage:  img.image,
-          //         // ),
-
-                  Text(doc.get("topicName"),
+    workCategoryList.add(Column(
+      children: [
+        Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(categoryName,
                     style: TextStyle(
                       fontWeight: FontWeight.normal,
                       fontSize: 16,
                       color: Colors.black45,
-                    ),)
-          //       ]
-          //     ),
-          //   )
-          // )
-
-
-        );
-      });
-    });
-
-    workCategoryList.add(Container(
-      height:40,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: topicList,
-      ),
+                    ),
+                  )),
+            )),
+        Container(
+          color: Colors.blueAccent,
+          height: 140,
+          width: 500,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: topicList,
+          ),
+        ),
+      ],
     ));
 
     //topicList.clear();
